6ddff6c31d1fd887c6f85d3470fef894f27979ee
SamirSalmanov <samir_salmanov_1988@mail.ru>
2020-11-30 02:45:17 -0800

    carnav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+window.onmousemove=function(e){
+
+    let moveNum=(e.pageX/innerWidth)*180;
+    let moveNum2=(e.pageY/innerHeight)*180;
+    
+    let newNum=((e.pageX - innerWidth/2)/(innerWidth/2));
+    let newNum2=((e.pageY - innerHeight/2)/(innerHeight/2));
+    
+    document.querySelector(".myLogo").style.transform=`translate(${-50 - (newNum*10)}%,${-50 - (newNum2*10)}%)`
+    }
+
 $("#accordion button").click(function(){
     $("#accordion p").stop().slideUp()
     $(this).next().stop().slideToggle()
@@ -24,7 +35,7 @@ $(".nav-ico").on("click",function(){
     $(this).find("#ico-rem").toggle()
     $(this).find("#ico-bar").toggle()
 
-    $(".mobile-down-menu").toggleClass(500)
+    $(".mobile-down-menu").slideToggle(500)
 })
 $(".menu-mobile .mobile-down-menu .set a").click(function(){
     // $(".set i").removeClass("fas fa-chevron-down");
@@ -38,7 +49,10 @@ setTimeout(() => {
     $("#slider1 .slide-img2").delay(1000).fadeIn()
 
 }, 500);
+
+
 $('.sld').textillate({
+
     // the default selector to use when detecting multiple texts to animate
     selector: '.texts',
   
@@ -105,4 +119,97 @@ $('.sld').textillate({
   
     // set the type of token to animate (available types: 'char' and 'word')
     type: 'char'
+    
   });
+
+
+
+
+let current=0;
+$(".ico-next").click(function(){
+    $('.sld').textillate('out');
+    $('.sld').textillate('in');
+
+
+    $('.sld').textillate({
+
+    // the default selector to use when detecting multiple texts to animate
+    selector: '.texts',
+  
+    // enable looping
+    loop: false,
+  
+    // sets the minimum display time for each text before it is replaced
+    minDisplayTime: 2000,
+  
+    // sets the initial delay before starting the animation
+    // (note that depending on the in effect you may need to manually apply
+    // visibility: hidden to the element before running this plugin)
+    initialDelay: 0,
+  
+    // set whether or not to automatically start animating
+    autoStart: true,
+  
+    // custom set of 'in' effects. This effects whether or not the
+    // character is shown/hidden before or after an animation
+    inEffects: [],
+  
+    // custom set of 'out' effects
+    outEffects: [ 'hinge' ],
+  
+    // in animation settings
+    in: {
+        // set the effect name
+      effect: 'fadeInLeftBig',
+  
+      // set the delay factor applied to each consecutive character
+      delayScale: 1.5,
+  
+      // set the delay between each character
+      delay: 50,
+  
+      // set to true to animate all the characters at the same time
+      sync: false,
+  
+      // randomize the character sequence
+      // (note that shuffle doesn't make sense with sync = true)
+      shuffle: false,
+  
+      // reverse the character sequence
+      // (note that reverse doesn't make sense with sync = true)
+      reverse: false,
+  
+      // callback that executes once the animation has finished
+      callback: function () {}
+    },
+  
+    // out animation settings.
+    out: {
+      effect: 'hinge',
+      delayScale: 1.5,
+      delay: 50,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+  
+    // callback that executes once textillate has finished
+    callback: function () {},
+  
+    // set the type of token to animate (available types: 'char' and 'word')
+    type: 'char'
+    
+  });
+
+
+
+   current=(current+1+$(".slider-item").lenth)%$(".slider-item").length
+   $(".slider-item").removeClas(".sld-active")
+   $(".slider-item").eq(current).addClass("sld-active")
+   $(".slider-item")
+})
+
+
+
+  
